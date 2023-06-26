7713b409958daef1fba44d68fcb31c5ebe1f719d
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-06-26 12:18:59 +0100

    Update 12-createReportObject.js

--- 0x00-ES6_basic/12-createReportObject.js
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -1,1 +1,8 @@
-qwertyu
+export default function createReportObject(employeesList) {
+  return {
+    allEmployees: employeesList,
+    getNumberOfDepartments(employees) {
+      return Object.keys(employees).length;
+    },
+  };
+}
