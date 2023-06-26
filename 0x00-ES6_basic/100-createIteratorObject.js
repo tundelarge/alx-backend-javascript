01748dd6fa218ca56f9fdad7e00d1c74a9525eeb
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-06-26 12:20:48 +0100

    Update 100-createIteratorObject.js

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,1 +1,9 @@
-qwertyu
+export default function createIteratorObject(report) {
+  return (function* _() {
+    for (const department of Object.values(report.allEmployees)) {
+      for (const employee of department) {
+        yield employee;
+      }
+    }
+  }());
+}
