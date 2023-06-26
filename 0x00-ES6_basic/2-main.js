d1eee4ceac637f0a5b8acf1abbc74c077cdc10c3
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-06-26 12:00:10 +0100

    Update 2-main.js

--- 0x00-ES6_basic/2-main.js
+++ 0x00-ES6_basic/2-main.js
@@ -1,1 +1,5 @@
-qwertyu
+import getNeighborhoodsList from './2-arrow.js';
+
+const neighborhoodsList = new getNeighborhoodsList();
+const res = neighborhoodsList.addNeighborhood('Noe Valley');
+console.log(res);
