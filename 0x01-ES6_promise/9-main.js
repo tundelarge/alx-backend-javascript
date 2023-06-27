8d40688e1980511cc273ec0e41b199eeefdcdcb9
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-06-27 15:58:56 +0100

    Update 9-main.js

--- 0x01-ES6_promise/9-main.js
+++ 0x01-ES6_promise/9-main.js
@@ -1,1 +1,5 @@
-qwerty
+import guardrail from './9-try';
+import divideFunction from './8-try';
+
+console.log(guardrail(() => { return divideFunction(10, 2)}));
+console.log(guardrail(() => { return divideFunction(10, 0)}));
