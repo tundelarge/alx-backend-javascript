2217666f778f8fc685901e1d945693048577cc09
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-06-27 15:58:13 +0100

    Update 8-try.js

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,1 +1,6 @@
-qwertyu
+export default function divideFunction(numerator, denominator) {
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+  return (numerator / denominator);
+}
