4c70b843355f07b3cb035a6eb3ed20bd64bd2a27
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-06-27 15:50:31 +0100

    Update 3-all.js

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,9 @@
-qwertyu
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  const data = [uploadPhoto(), createUser()];
+
+  return Promise.all(data)
+    .then((data) => console.log(`${data[0].body} ${data[1].firstName} ${data[1].lastName}`),
+      () => console.log('Signup system offline'));
+}
