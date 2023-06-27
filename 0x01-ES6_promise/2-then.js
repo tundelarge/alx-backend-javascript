2d34504fabf03670faf8e05dafea331f7008b22c
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-06-27 15:49:14 +0100

    Update 2-then.js

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,10 @@
-qwerty
+export default function handleResponseFromAPI(promise) {
+  const body = { status: 200, body: 'success' };
+
+  return promise
+    .then(() => body)
+    .catch(() => new Error())
+    .finally(() => {
+      console.log('Got a response from the API');
+    });
+}
