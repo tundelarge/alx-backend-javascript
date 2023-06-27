4a450d8ca809482f85e6dda2ae455d6980ec2df6
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-06-27 15:59:27 +0100

    Update 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,13 @@
-qwertyu
+export default function guardrail(mathFunction) {
+  const queue = [];
+  let mathFunc;
+
+  try {
+    mathFunc = mathFunction();
+  } catch (error) {
+    mathFunc = error.toString();
+  }
+  queue.push(mathFunc);
+  queue.push('Guardrail was processed');
+  return queue;
+}
