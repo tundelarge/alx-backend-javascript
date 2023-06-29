878e23ea99d2b4b0158036482f19408dde3f5abc
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-06-29 17:40:46 +0100

    Update 5-building.js

--- 0x02-ES6_classes/5-building.js
+++ 0x02-ES6_classes/5-building.js
@@ -1,1 +1,22 @@
-qwertyu
+/* eslint-disable no-underscore-dangle */
+class Building {
+  constructor(sqft) {
+    if (
+      this.constructor !== Building
+      && typeof this.evacuationWarningMessage !== 'function'
+    ) {
+      throw Error(
+        'Class extending Building must override evacuationWarningMessage',
+      );
+    }
+
+    this._sqft = sqft;
+  }
+
+  // sqft
+  get sqft() {
+    return this._sqft;
+  }
+}
+
+export default Building;
