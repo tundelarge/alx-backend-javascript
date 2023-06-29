9492f4b134a41fb3f6ec4e1dbe03985b7378f2f3
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-06-29 17:46:33 +0100

    Update 100-evcar.js

--- 0x02-ES6_classes/100-evcar.js
+++ 0x02-ES6_classes/100-evcar.js
@@ -1,1 +1,14 @@
-qwertyu
+import Car from './10-car';
+
+class EVCar extends Car {
+  constructor(brand = '', motor = '', color = '', range = '') {
+    super(brand, motor, color);
+    this._range = range;
+  }
+
+  static get [Symbol.species]() {
+    return Car;
+  }
+}
+
+export default EVCar;
