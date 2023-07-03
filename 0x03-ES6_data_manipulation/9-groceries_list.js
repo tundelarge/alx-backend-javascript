1ec9a4a3d5c7916aa80b5caa48d4283f31dd03de
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-07-03 20:46:02 +0100

    Update 9-groceries_list.js

--- 0x03-ES6_data_manipulation/9-groceries_list.js
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -1,1 +1,11 @@
-qwertyu
+export default function groceriesList() {
+  return new Map(
+    [
+      ['Apples', 10],
+      ['Tomatoes', 10],
+      ['Pasta', 1],
+      ['Rice', 1],
+      ['Banana', 5],
+    ],
+  );
+}
