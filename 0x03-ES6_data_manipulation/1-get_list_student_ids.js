fea25cc020a0de0d7a8e843cd29f8dc535a8983c
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-07-03 20:30:23 +0100

    Update 1-get_list_student_ids.js

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,1 +1,7 @@
-qwertyui
+export default function getListStudentIds(objectsArray) {
+  if (!Array.isArray(objectsArray)) {
+    return [];
+  }
+  const ids = objectsArray.map((x) => x.id);
+  return ids;
+}
