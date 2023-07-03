425f46af6ca1e1ee01664ea5c2f2f99e756e8d34
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-07-03 20:41:41 +0100

    Update 3-get_ids_sum.js

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,6 @@
-qwertyu
+export default function getStudentIdsSum(studentData) {
+  const numberOfStudents = studentData.reduce(
+    (cumulative, currentValue) => currentValue.id + cumulative, 0,
+  );
+  return numberOfStudents;
+}
