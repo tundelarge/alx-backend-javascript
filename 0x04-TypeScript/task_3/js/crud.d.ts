9a2c5dcf70a98422efa429298682b9db13485c41
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-07-05 07:57:16 +0100

    Update crud.d.ts

--- 0x04-TypeScript/task_3/js/crud.d.ts
+++ 0x04-TypeScript/task_3/js/crud.d.ts
@@ -1,1 +1,7 @@
-qwertyu
+
+import {RowElement,RowID } from './interface';
+
+
+declare function insertRow(row: RowElement): number;
+declare function deleteRow(rowId: RowID): void;
+declare function updateRow(rowId: RowID, row: RowElement): number;
