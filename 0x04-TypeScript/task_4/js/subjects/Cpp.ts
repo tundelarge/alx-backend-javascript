9c63420e150c79049a5cdc27d81fcce30c1cb5de
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-07-05 08:28:28 +0100

    Update Cpp.ts

--- 0x04-TypeScript/task_4/js/subjects/Cpp.ts
+++ 0x04-TypeScript/task_4/js/subjects/Cpp.ts
@@ -1,1 +1,14 @@
-qwertyu
+namespace Subjects {
+    export interface Teacher {
+      experienceTeachingC?: number;
+    }
+  
+    export class Cpp extends Subject {
+      getRequirements(): string {
+        return `Here is the list of requirements for Cpp`;
+      }
+      getAvailableTeacher(): string {
+        return this.teacher.experienceTeachingC > 0 ? `Available Teacher: ${this.teacher.firstName}` : 'No available teacher';
+      }
+    }  
+  }
