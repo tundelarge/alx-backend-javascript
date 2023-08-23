19938728a843693931faceb9ddee8b915fca0ebe
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-08-23 15:54:39 +0100

    Update 0-calcul.js

--- 0x06-unittests_in_js/0-calcul.js
+++ 0x06-unittests_in_js/0-calcul.js
@@ -1,1 +1,8 @@
-qwertyu
+function calculateNumber(a, b) {
+    const an = Math.round(a);
+    const bn = Math.round(b);
+    const c = an + bn;
+    return c;
+}
+
+module.exports = calculateNumber;
