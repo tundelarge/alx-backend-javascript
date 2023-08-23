731ad8e9a136dc6fcb32a789884ca54542ccd33a
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-08-23 16:24:48 +0100

    Update 3-payment.js

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,1 +1,8 @@
-qwertyu
+const Utils = require("./utils");
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    const result = Utils.calculateNumber("SUM", totalAmount, totalShipping);
+    console.log(`The total is: ${result}`);
+}
+
+module.exports = sendPaymentRequestToApi;
