c9a7ab0a008ff9ba1cc6b70ff5ed2729c3f9987a
Ayotunde Esan <110999627+tundelarge@users.noreply.github.com>
2023-08-23 16:28:50 +0100

    Update 6-payment_token.js

--- 0x06-unittests_in_js/6-payment_token.js
+++ 0x06-unittests_in_js/6-payment_token.js
@@ -1,1 +1,9 @@
-qwertyu
+function getPaymentTokenFromAPI(success) {
+    if (success) {
+	return new Promise((resolve) => {
+          resolve({data: 'Successful response from the API' });
+	});
+    }
+}
+
+module.exports = getPaymentTokenFromAPI;
